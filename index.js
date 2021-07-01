ae8a682bffa700742db8fa80ad65f3b1a36047b2
breannams <breannamarie_s@hotmail.com>
2021-07-01 19:38:40 -0400

    commit tree 3bcf02f2374ab0687be29bda207477e824d239e3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+let driver = {name: "idk"}
+
+// function updateDriverWithKeyAndValue(driver, key, value){
+//     const newDriver = {...driver}
+//     newDriver[key] = value
+//     return newDriver
+// }
+
+// ^^ works the same as vvv
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  return Object.assign({}, driver, {[key]: value})
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+const newDriver = Object.assign({}, driver)
+    delete newDriver[key]
+    return newDriver
+}
+
+function  destructivelyDeleteFromDriverByKey(driver, key){
+    Object.assign({}, driver);
+    delete driver[key];
+    return driver
+}
+
+
+///    const newDriver = delete driver.key
+//return newDriver
